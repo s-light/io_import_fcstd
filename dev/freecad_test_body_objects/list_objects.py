e04262fe00f6c9b47617760af37085fb53f7518d
s-light <git@s-light.eu>
2019-10-25 20:07:33 +0200

    [dev-example] added minimal body-test

---
+++ dev/freecad_test_body_objects/list_objects.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+List Objects.
+
+Stand-alone
+"""
+
+import sys
+import os
+
+
+path_to_freecad = "/usr/lib/freecad-daily-python3/lib/FreeCAD.so"
+
+
+def append_freecad_path():
+    """Append the FreeCAD path."""
+    global path_to_freecad
+    if os.path.exists(path_to_freecad):
+        if os.path.isfile(path_to_freecad):
+            path_to_freecad = os.path.dirname(path_to_freecad)
+        print("Configured FreeCAD path:", path_to_freecad)
+        if path_to_freecad not in sys.path:
+            sys.path.append(path_to_freecad)
+    else:
+        print("FreeCAD path is not correct.")
+
+
+try:
+    try:
+        import FreeCAD
+    except ModuleNotFoundError:
+        append_freecad_path()
+    import FreeCAD  # noqa
+    print("FreeCAD version:", FreeCAD.Version())
+except ModuleNotFoundError as e:
+    print("FreeCAD import failed.", e)
+
+
+# ******************************************
+
+def print_obj_header(
+    pre_line="",
+    show_lists=False,
+    show_list_details=False,
+):
+    """Print header for objects list."""
+    print(
+        pre_line +
+        "{:<15} {:<25} {:<25}"
+        "".format("Label", "Name", "TypeId"),
+        end=''
+    )
+    if show_lists:
+        print(
+            "{:>2} {:>2} {:>2} {:>2}"
+            "".format(
+                "P",
+                "I",
+                "O",
+                "G",
+            ),
+            end=''
+        )
+        if show_list_details:
+            print(
+                (
+                    "    {:<10}" * 4
+                ).format(
+                    '[Parents]',
+                    '[InList]',
+                    '[OutList]',
+                    '[Group]'
+                ),
+                end=''
+            )
+    print()
+
+
+def print_obj(
+    obj,
+    pre_line="",
+    show_lists=False,
+    show_list_details=False,
+    end="\n",
+):
+    """Print object nicely formated."""
+    print(
+        pre_line +
+        "{:<25} {:<15} {:<25}"
+        "".format(obj.Label, obj.Name, obj.TypeId),
+        end=''
+    )
+    if show_lists:
+        group_count = '_'
+        if hasattr(obj, 'Group'):
+            group_count = len(obj.Group)
+        print(
+            "{:>2} {:>2} {:>2} {:>2}"
+            "".format(
+                len(obj.Parents),
+                len(obj.InList),
+                len(obj.OutList),
+                group_count
+            ),
+            end=''
+        )
+        if show_list_details:
+            group = None
+            if hasattr(obj, 'Group'):
+                group = obj.Group
+            print(
+                (
+                    "    {:<10}" * 4
+                ).format(
+                    str(obj.Parents),
+                    str(obj.InList),
+                    str(obj.OutList),
+                    str(group)
+                ),
+                end=''
+            )
+    print("", end=end)
+
+
+def print_objects(
+    objects,
+    pre_line="",
+    pre_list_entry="* ",
+    show_lists=False,
+    show_list_details=False,
+):
+    """Print objects list."""
+    pre_list_entry_space = " "*len(pre_list_entry)
+    print_obj_header(
+        pre_line=pre_line + pre_list_entry_space,
+        show_lists=show_lists,
+        show_list_details=show_list_details,
+    )
+    for obj in objects:
+        print_obj(
+            obj,
+            pre_line=pre_line + pre_list_entry,
+            show_lists=show_lists,
+            show_list_details=show_list_details,
+        )
+
+
+def print_obj_with_label(doc, label):
+    """Print object with given label."""
+    obj = doc.getObjectsByLabel(label)
+    # print(obj)
+    if len(obj) > 0:
+        obj = obj[0]
+        print_obj(obj)
+    else:
+        print("object with label '{}' not found.".format(label))
+
+
+# ******************************************
+#
+#            Main experimetns
+#
+# ******************************************
+
+doc = FreeCAD.open(
+    "./BodyTest.FCStd"
+)
+docname = doc.Name
+
+# ******************************************
+print("~"*42)
+objects = doc.Objects
+print("doc.Objects", len(objects))
+print_objects(objects)
+print("~"*42)
+
+print_obj_with_label(doc, "BodyTest_Part")
+print_obj_with_label(doc, "Body_Hex")
+print_obj_with_label(doc, "Sphere_Sun")
+
+print("~"*42)
+print("tests done :-)")
+
+FreeCAD.closeDocument(docname)
---
+++ dev/freecad_test_body_objects/list_objects_copypaste.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+List Objects.
+
+Copy&Paste this complete script into the FreeCAD Python console!
+"""
+
+import FreeCAD
+
+
+def print_obj_header(
+    pre_line="",
+    show_lists=False,
+    show_list_details=False,
+):
+    """Print header for objects list."""
+    print(
+        pre_line +
+        "{:<15} {:<25} {:<25}"
+        "".format("Label", "Name", "TypeId"),
+        end=''
+    )
+    if show_lists:
+        print(
+            "{:>2} {:>2} {:>2} {:>2}"
+            "".format(
+                "P",
+                "I",
+                "O",
+                "G",
+            ),
+            end=''
+        )
+        if show_list_details:
+            print(
+                (
+                    "    {:<10}" * 4
+                ).format(
+                    '[Parents]',
+                    '[InList]',
+                    '[OutList]',
+                    '[Group]'
+                ),
+                end=''
+            )
+    print()
+
+
+def print_obj(
+    obj,
+    pre_line="",
+    show_lists=False,
+    show_list_details=False,
+    end="\n",
+):
+    """Print object nicely formated."""
+    print(
+        pre_line +
+        "{:<25} {:<15} {:<25}"
+        "".format(obj.Label, obj.Name, obj.TypeId),
+        end=''
+    )
+    if show_lists:
+        group_count = '_'
+        if hasattr(obj, 'Group'):
+            group_count = len(obj.Group)
+        print(
+            "{:>2} {:>2} {:>2} {:>2}"
+            "".format(
+                len(obj.Parents),
+                len(obj.InList),
+                len(obj.OutList),
+                group_count
+            ),
+            end=''
+        )
+        if show_list_details:
+            group = None
+            if hasattr(obj, 'Group'):
+                group = obj.Group
+            print(
+                (
+                    "    {:<10}" * 4
+                ).format(
+                    str(obj.Parents),
+                    str(obj.InList),
+                    str(obj.OutList),
+                    str(group)
+                ),
+                end=''
+            )
+    print("", end=end)
+
+
+def print_objects(
+    objects,
+    pre_line="",
+    pre_list_entry="* ",
+    show_lists=False,
+    show_list_details=False,
+):
+    """Print objects list."""
+    pre_list_entry_space = " "*len(pre_list_entry)
+    print_obj_header(
+        pre_line=pre_line + pre_list_entry_space,
+        show_lists=show_lists,
+        show_list_details=show_list_details,
+    )
+    for obj in objects:
+        print_obj(
+            obj,
+            pre_line=pre_line + pre_list_entry,
+            show_lists=show_lists,
+            show_list_details=show_list_details,
+        )
+
+
+def print_obj_with_label(doc, label):
+    """Print object with given label."""
+    obj = doc.getObjectsByLabel(label)
+    # print(obj)
+    if len(obj) > 0:
+        obj = obj[0]
+        print_obj(obj)
+    else:
+        print("object with label '{}' not found.".format(label))
+
+
+# ******************************************
+#
+#            Main experimetns
+#
+# ******************************************
+
+doc = FreeCAD.ActiveDocument
+docname = doc.Name
+
+# ******************************************
+objects = doc.Objects
+print("doc.Objects", len(objects))
+print_objects(objects)
+
+# ******************************************
+
+print_obj_with_label(doc, "BodyTest_Part")
+print_obj_with_label(doc, "Body_Hex")
+print_obj_with_label(doc, "Sphere_Sun")
+
+# ******************************************
+print("tests done :-)")
