802307892b54b83d45f8b28d4968f5e43d7e2c39
s-light <git@s-light.eu>
2021-04-19 16:53:12 +0200

    freecad_helper - fix ARCH `Hosts` sub obj handling (and black formating)

--- freecad_helper.py
+++ freecad_helper.py
@@ -5,49 +5,49 @@
 
 
 def print_obj_header(
-    pre_line="",
-    show_lists=False,
-    show_list_details=False,
+    pre_line="", show_lists=False, show_list_details=False,
 ):
     """Print header for objects list."""
     print(
-        pre_line +
-        "{:<15} {:<25} {:<25}"
-        "".format("Label", "Name", "TypeId"),
-        end=''
+        pre_line + "{:<25} {:<15} {:<25}" "".format("Label", "Name", "TypeId"), end="",
     )
     if show_lists:
-        print(
-            "{:>2} {:>2} {:>2} {:>2}"
-            "".format(
-                "P",
-                "I",
-                "O",
-                "G",
-            ),
-            end=''
-        )
+        print("{:>2} {:>2} {:>2} {:>2}" "".format("P", "I", "O", "G",), end="")
         if show_list_details:
             print(
-                (
-                    "    {:<10}" * 4
-                ).format(
-                    '[Parents]',
-                    '[InList]',
-                    '[OutList]',
-                    '[Group]'
+                ("    {:<10}" * 4).format(
+                    "[Parents]", "[InList]", "[OutList]", "[Group]", "[Hosts]"
                 ),
-                end=''
+                end="",
             )
     print()
 
 
+def format_obj_show_lists(obj, show_list_details):
+    result = ""
+    group_count = "_"
+    if hasattr(obj, "Group"):
+        group_count = len(obj.Group)
+    hosts_count = "_"
+    if hasattr(obj, "Hosts"):
+        hosts_count = len(obj.Hosts)
+    result += "{:>2} {:>2} {:>2} {:>2} {:>2}" "".format(
+        len(obj.Parents), len(obj.InList), len(obj.OutList), group_count, hosts_count,
+    )
+    if show_list_details:
+        group = None
+        if hasattr(obj, "Group"):
+            group = obj.Group
+        hosts = None
+        if hasattr(obj, "Hosts"):
+            hosts = obj.Hosts
+        result += ("    {:<10}" * 4).format(
+            str(obj.Parents), str(obj.InList), str(obj.OutList), str(group), str(hosts),
+        )
+
+
 def format_obj(
-    obj,
-    pre_line="",
-    show_lists=False,
-    show_list_details=False,
-    tight_format=False
+    obj, pre_line="", show_lists=False, show_list_details=False, tight_format=False
 ):
     """Print object nicely formated."""
     result = ""
@@ -63,46 +63,14 @@ def format_obj(
     obj_format = "{:<25} {:<15} {:<25}"
     if tight_format:
         obj_format = "'{}' ('{}' <{}>)"
-    result += (
-        pre_line +
-        obj_format.format(obj_label, obj_name, obj_type)
-    )
+    result += pre_line + obj_format.format(obj_label, obj_name, obj_type)
     if show_lists:
-        group_count = '_'
-        if hasattr(obj, 'Group'):
-            group_count = len(obj.Group)
-        result += (
-            "{:>2} {:>2} {:>2} {:>2}"
-            "".format(
-                len(obj.Parents),
-                len(obj.InList),
-                len(obj.OutList),
-                group_count
-            )
-        )
-        if show_list_details:
-            group = None
-            if hasattr(obj, 'Group'):
-                group = obj.Group
-            result += (
-                (
-                    "    {:<10}" * 4
-                ).format(
-                    str(obj.Parents),
-                    str(obj.InList),
-                    str(obj.OutList),
-                    str(group)
-                )
-            )
+        result += format_obj_show_lists(obj, show_list_details)
     return result
 
 
 def print_obj(
-    obj,
-    pre_line="",
-    show_lists=False,
-    show_list_details=False,
-    end="\n",
+    obj, pre_line="", show_lists=False, show_list_details=False, end="\n",
 ):
     print(
         format_obj(
@@ -111,7 +79,7 @@ def print_obj(
             show_lists=show_lists,
             show_list_details=show_list_details,
         ),
-        end=end
+        end=end,
     )
 
 
@@ -123,7 +91,7 @@ def print_objects(
     show_list_details=False,
 ):
     """Print objects list."""
-    pre_list_entry_space = " "*len(pre_list_entry)
+    pre_list_entry_space = " " * len(pre_list_entry)
     print_obj_header(
         pre_line=pre_line + pre_list_entry_space,
         show_lists=show_lists,
@@ -141,26 +109,22 @@ def print_objects(
 # ****************************************
 
 
-def filtered_objects(
-    objects,
-    typeid_filter_list=None,
-    include_only_visible=False
-):
+def filtered_objects(objects, typeid_filter_list=None, include_only_visible=False):
     """Filter list of objects."""
     if typeid_filter_list is None:
         typeid_filter_list = [
-            'App::Line',
-            'App::Plane',
-            'App::Origin',
+            "App::Line",
+            "App::Plane",
+            "App::Origin",
             # 'GeoFeature',
             # 'PartDesign::CoordinateSystem',
             # 'Sketcher::SketchObject',
         ]
     result_objects = []
     for obj in objects:
-        if (obj.TypeId not in typeid_filter_list):
+        if obj.TypeId not in typeid_filter_list:
             if include_only_visible:
-                if hasattr(obj, 'Visibility'):
+                if hasattr(obj, "Visibility"):
                     if obj.Visibility:
                         result_objects.append(obj)
                 else:
@@ -177,26 +141,27 @@ def filtered_objects(
 
 def get_filtered_objects(doc, typeid_filter_list=None):
     """Get filterd list of objects."""
-    result_objects = filtered_objects(
-        doc.Objects,
-        typeid_filter_list
-    )
+    result_objects = filtered_objects(doc.Objects, typeid_filter_list)
     return result_objects
 
 
 def get_root_objects(doc, filter_list=[]):
     """Get root list of objects."""
     typeid_filter_list = [
-        'App::Line',
-        'App::Plane',
-        'App::Origin',
+        "App::Line",
+        "App::Plane",
+        "App::Origin",
     ]
     typeid_filter_list = typeid_filter_list + filter_list
     result_objects = []
     for obj in doc.Objects:
         if obj.TypeId not in typeid_filter_list:
-            if (len(obj.Parents) == 0):
-                result_objects.append(obj)
+            if len(obj.Parents) == 0:
+                if hasattr(obj, "Hosts"):
+                    if len(obj.Hosts) == 0:
+                        result_objects.append(obj)
+                else:
+                    result_objects.append(obj)
     return result_objects
 
 
@@ -204,6 +169,7 @@ def get_root_objects(doc, filter_list=[]):
 # `is_toplevel_in_list` and `get_toplevel_objects`
 # from forum post 'Get highest objects of model' by kbwbe
 # https://forum.freecadweb.org/viewtopic.php?p=338214&sid=a6dd59fe66c1d807f8537f192fdb14dc#p338214
+
 
 def is_toplevel_in_list(lst):
     """Check if objects in list are at top level."""
@@ -232,9 +198,9 @@ def get_toplevel_objects(doc):
             # perhaps pairs of Clone/Bodies
             if len(ob.InList) % 2 == 0:
                 for o in ob.InList:
-                    if o.Name.startswith('Clone'):
+                    if o.Name.startswith("Clone"):
                         numClones += 1
-                    elif o.Name.startswith('Body'):
+                    elif o.Name.startswith("Body"):
                         numBodies += 1
                     else:
                         invalidObjects = True
